a6d50a72e957f12bb20ed68d564ca78ebc9a4166
irvan <irvanprima2102@gmail.com>
2022-10-31 17:51:33 +0700

    add button hello

--- AnProject/Form1.cs
+++ AnProject/Form1.cs
@@ -6,5 +6,10 @@ namespace AnProject
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Hello";
+        }
     }
 }
